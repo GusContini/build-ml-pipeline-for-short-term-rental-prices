681c9cc409da3a8a667a9f4144393eaeda90f1b3
GusContini <gcontini.torres@gmail.com>
2024-03-17 18:00:45 -0300

    adjusted lat&long range

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -26,6 +26,9 @@ def go(args):
     logger.info("Overwriting values outside min and max band")
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    logger.info("Adjusting lat & long range")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Saving the output to a CSV file")
     filename = "clean_sample.csv" 
